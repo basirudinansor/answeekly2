d772343d7453a986f59aae9a6207d41288046a07
basirudinansor <anzhrohmm@gmail.com>
2026-06-11 16:30:37 +0700

    fix: table mahasiswa

--- mahasiswa.php
+++ mahasiswa.php
@@ -12,32 +12,45 @@
     <table border="1" align="center" cellspacing="0" cellpadding="10px">
         <tr>
             <td>
-                <a href="index.html">Home</a>
+                <a href="index.php">Home</a>
             </td>
             <td>
-                <a href="profile.html">Profile</a>
+                <a href="profile.php">Profile</a>
             </td>
             <td>
-                <a href="contact.html">Contact</a>
+                <a href="contact.php">Contact</a>
             </td>
             <td>
-                <a href="mahasiswa.html">Data Mahasiswa</a>
+                <a href="mahasiswa.php">Data Mahasiswa</a>
             </td>
         </tr>
     </table>
     <br><br>
     <h2>Data Mahasiswa</h2>
+    <a href="tambahdata.php"><button>Tambah Data</button></a>
     <table border="1" cellpadding="5px">
         <tr>
-            <td rowspan="2">Baris 1 dan 2, Kolom 1</td>
-            <td colspan="2">Baris 1, Kolom 2 dan 3</td>
-            <!-- <td>Baris 1, Kolom 2</td> -->
+            <th>No</th>
+            <th>Nama</th>  
+            <th>NIM</th>
+            <th>Program Studi</th>  
+            <th>Email</th>
+            <th>Nomor Whatsapp</th>
+            <th>Foto</th>
+            <th>Aksi</th>        
         </tr>
         <tr>
-            <!-- <td rowspan="2">Baris 1, Kolom 1</td> -->
-            <td>Baris 2, Kolom 2</td>
-            <td>Baris 2, Kolom 3</td>
-            <!-- <td>Baris 2, Kolom 3</td> -->
+            <td>1</td>
+            <td>Fiqri Ilhami</td>
+            <td>123123123123</td>
+            <td>Teknologi Informasi</td>
+            <td>fiqri@gmail.com</td>
+            <td>087766552312</td>
+            <td><img src="images/logo.jpg" width="60px"></td>
+            <td>
+                <a href="editdata.php"><button>Edit</button></a> |
+                <a href="deletedata.php"><button>Hapus</button></a>
+            </td>
         </tr>
     </table>
 </body>
